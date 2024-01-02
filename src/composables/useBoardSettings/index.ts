a56247d52b3654650a8edf0c63eb06200a12da10
Oktopoid <orson_wells40@hotmail.com>
2024-01-02 19:30:09 +0100

    Added ref support

---
+++ src/composables/useBoardSettings/index.ts
@@ -0,0 +1,6 @@
+import { configStore } from '@/stores/configStore'
+import { useSetCanvasSize } from '@/composables'
+
+export function useBoardSettings() {
+  const gameConfig = configStore()
+}
--- src/composables/useSetCanvasSize/index.ts
+++ src/composables/useSetCanvasSize/index.ts
@@ -1,33 +1,41 @@
 // HTMLImageElement
-import { ref } from 'vue'
+import { ref, toValue, type Ref } from 'vue'
 interface CanvasSizeOptions {
-  width: number | undefined
-  height: number | undefined
-  maxWidth: number
-  maxHeight: number
-  resolution: number
+  width: number | Ref<number> | undefined | Ref<undefined>
+  height: number | Ref<number> | undefined | Ref<undefined>
+  maxWidth: number | Ref<number>
+  maxHeight: number | Ref<number>
+  resolution: number | Ref<number>
 }
 
 export function useSetCanvasSize(options: CanvasSizeOptions) {
+  let width = toValue(options.width)
+  let height = toValue(options.height)
+  let maxWidth = toValue(options.maxWidth)
+  let maxHeight = toValue(options.maxHeight)
+  let resolution = toValue(options.resolution)
+
   // Handle negative numbers
-  options.width = options.width !== undefined && options.width > 0 ? options.width : 0
-  options.height = options.height !== undefined && options.height > 0 ? options.height : 0
-  options.maxWidth = options.maxWidth > 0 ? options.maxWidth : 0
-  options.maxHeight = options.maxHeight > 0 ? options.maxHeight : 0
-  options.resolution = options.resolution > 0 ? options.resolution : 0
+  width = width !== undefined && width > 0 ? width : 0
+  height = height !== undefined && height > 0 ? height : 0
+  maxWidth = maxWidth > 0 ? maxWidth : 0
+  maxHeight = maxHeight > 0 ? maxHeight : 0
+  resolution = resolution > 0 ? resolution : 0
 
   // Determine the larger side of the image and the corresponding max dimension
-  const largerSide = options.width > options.height ? 'width' : 'height'
-  const maxDimension = largerSide === 'width' ? options.maxWidth : options.maxHeight
+  const largerSide = width > height ? width : height
+  const maxDimension = largerSide === width ? maxWidth : maxHeight
 
   // Calculate the ratio based on the larger side
-  const ratio = maxDimension / (options[largerSide] || 1)
+  const ratio = maxDimension / (largerSide || 1)
 
-  const newWidth = ref(Math.round((options.width || 0) * ratio))
-  const newHeight = ref(Math.round((options.height || 0) * ratio))
+  const newWidth = ref(Math.round((width || 0) * ratio))
+  const newHeight = ref(Math.round((height || 0) * ratio))
 
-  const cols = ref(options.resolution > 0 ? Math.floor(newWidth.value / options.resolution) : 0)
-  const rows = ref(options.resolution > 0 ? Math.floor(newHeight.value / options.resolution) : 0)
+  const cols = ref(resolution > 0 ? Math.floor(newWidth.value / resolution) : 0)
+  const rows = ref(
+    resolution > 0 ? Math.floor(newHeight.value / resolution) : 0
+  )
 
   return { width: newWidth, height: newHeight, cols, rows }
 }
